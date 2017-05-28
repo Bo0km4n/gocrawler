16ce0dfeaec955d587045ba19e1fa03160351a34
川部勝也 <al14031@shibaura-it.ac.jp>
2017-05-28 21:41:15 +0900

    add getlink method

--- checktype.go
+++ checktype.go
@@ -1,37 +1,32 @@
-package crawler
+package main
 
 import (
 	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"io/ioutil"
-	"os"
+	"reflect"
 )
 
-func Crawl() error {
+func main() {
 	doc, err := goquery.NewDocument("http://www.ise.shibaura-it.ac.jp")
 	if err != nil {
 		fmt.Println("url scraping failed")
 	}
 
-	res, err := doc.Html()
-	if err != nil {
-		fmt.Println("dom get failed")
-	}
-
+	fmt.Println(reflect.TypeOf(doc))
 	getLink(doc)
-	//ioutil.WriteFile("./sample.html", []byte(res), os.ModePerm)
-	return nil
 }
 
 func getLink(doc *goquery.Document) ([]string, error) {
 	var url_list []string
+	fmt.Println("test")
 	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
-		url, _ := s.Ttr("href")
+		url, _ := s.Attr("href")
 		url_list = append(url_list, url)
 	})
 	if len(url_list) > 256 {
 		return url_list[0:255], errors.New("url_list is too long")
 	}
+	fmt.Println(reflect.TypeOf(url_list))
 	return url_list, nil
 }
